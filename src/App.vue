f29ef5b931b14e4a5fc035e44a4f9c91fbbd2a86
Roya Shakeri <shakeri.work@gmail.com>
2022-05-29 20:00:07 +0430

    setup app

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <Stores  />
+  </div>
+</template>
+
+<script>
+import Stores from "./components/Stores";
+
+export default {
+  name: "App",
+  components: {
+    Stores
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
