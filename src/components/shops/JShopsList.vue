c6081cbce7e933a499cc2da1f151894bd943080a
Roya Shakeri <shakeri.work@gmail.com>
2022-06-02 15:08:48 +0430

    Added JShopsList component

---
+++ src/components/shops/JShopsList.vue
@@ -0,0 +1,38 @@
+<template>
+  <ul>
+    <li v-for="shop in list" 
+      :key="shop.id" 
+      :style="getStylePerCity(shop.cityColor)">
+      <JShopItem :text="shop.addressName | removeStr('Jumbo ')"/>
+    </li>
+  </ul>
+</template>
+
+<script>
+import JShopItem from "./JShopItem";
+
+export default {
+  name: "JShopsList",
+  components: {
+    JShopItem
+  },
+  props: {
+    list: {
+      type: Array,
+      required: true
+    }
+  },
+  data() {
+    return {
+    };
+  },
+  methods: {
+    getStylePerCity(cityColor) {
+      return { "background-color": cityColor };
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
