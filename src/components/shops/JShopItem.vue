aa581dbafc5fa648b9d9926adc338bd463cbf147
Roya Shakeri <shakeri.work@gmail.com>
2022-06-02 15:07:39 +0430

    Added JShopItem component

---
+++ src/components/shops/JShopItem.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+        {{text}}
+  </div>
+</template>
+
+<script>
+export default {
+  name: "JShopItem",
+  data() {
+    return {
+    };
+  },
+  props: {
+    text: {
+        type: String,
+        required: true
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
