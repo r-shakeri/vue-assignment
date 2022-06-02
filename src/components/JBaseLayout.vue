bfcb3cae627a6a89ff245e04f81d4ecd06a780d0
Roya Shakeri <shakeri.work@gmail.com>
2022-06-02 15:09:38 +0430

    Added JBaseLayout component

---
+++ src/components/JBaseLayout.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <header>
+    </header>
+    <main>
+      <slot></slot>
+    </main>
+    <footer>
+    </footer>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "JBaseLayout",
+  data() {
+    return {
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
