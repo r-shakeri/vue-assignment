c142ab29f4b7069ab92eed5267bca7e198a3fb06
Roya Shakeri <shakeri.work@gmail.com>
2022-06-02 15:05:26 +0430

    Added JSearch component

---
+++ src/components/JSearch.vue
@@ -0,0 +1,29 @@
+<template>
+  <section>
+    <input 
+      :value="value"
+      @input="$emit('input', $event.target.value)"
+      placeholder="search" />
+  </section>
+</template>
+
+<script>
+
+export default {
+  name: "JSearch",
+  props: {
+    value: {
+      type: String,
+      required: false
+    }
+  },
+  data() {
+    return {
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
